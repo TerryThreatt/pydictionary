b002614ef5bed9c2de02d650bf85f741f510652e
Terry Threatt <terry.threatt@gmail.com>
2019-08-19 10:23:37 -0500

    added some conditional handling

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import json
+from difflib import get_close_matches
 
 
 data = json.load(open("data.json"))
 
 def translate(w):
-    return data[w]   
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0: 
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w,data.keys())[0]) 
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist in dictionary. Please try a new word."
+        else: 
+            return "We didn't understand your entry."
+    else: 
+        return "The word doesn't exist in dictionary. Please try a new word."
 
 word = input("Enter word for definition: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output: 
+        print(item)
+else: 
+    print(output)
